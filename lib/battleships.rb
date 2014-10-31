8d5a5edd5253503cb5957eee1dde0ac451164821
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2014-10-31 15:59:05 +0000

    notes for pry interaction added

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -39,9 +39,37 @@ class Battleships < Sinatra::Base
     @cells[2][0] = 'D'
     @cells[0][1] = 'O'
     @cells[0][2] = 'X'
+    
     erb :board
   end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
+
+
+
+# grid = {}
+# ("A".."J").to_a.each{|l| (1..10).to_a.each {|n| grid["#{l}#{n}"] = "~" }}
+# grid.values.each_slice(10).to_a
+
+
+# [31] pry(main)> class Cell
+# [31] pry(main)*   def content
+# [31] pry(main)*     "~"
+# [31] pry(main)*   end  
+# [31] pry(main)* end  
+# => :content
+# [32] pry(main)> grid = {}
+# => {}
+# [33] pry(main)> ("A".."J").to_a.each{|l| (1..10).to_a.each {|n| grid["#{l}#{n}"] = Cell.new } }
+# => ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+# [34] pry(main)> grid
+
+# [35] pry(main)> grid.values
+# [36] pry(main)> Cell.new.content
+# => "~"
+# [37] pry(main)> grid.values.map { |cell| cell.content }
+# [40] pry(main)> grid.values.map { |cell| cell.content }.each_slice(10).to_a
+
+
